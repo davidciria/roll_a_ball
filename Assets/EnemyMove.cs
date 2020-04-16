ad2ab523be079606e5c409c44973d62b86b2e813
David Ciria <david.ciria01@estudiant.upf.edu>
2020-04-16 13:37:10 +0200

    Added jump feature

---
+++ Assets/EnemyMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+
+    private float direction = -1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(new Vector3(0.0f, 0.0f, direction)*Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            direction *= -1;
+        }
+    }
+}
